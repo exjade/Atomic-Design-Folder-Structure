89aa610a9837fd9e23240308f8de9454e794a5ca
Adrian Zamora <adrianzamora10@outlook.com>
2022-05-30 02:32:22 -0600

    Atom - Button + Stories

--- src/components/atomic/atoms/Button/Button.js
+++ src/components/atomic/atoms/Button/Button.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './button.css';
 
-export default function Button({ children }) {
+export default function Button({ type, children }) {
     return (
-        <button>
+        <button
+            type='button'
+            className={`
+            ${classNames('button', {
+                [`type-${type}`]: type,
+            })} 
+            h-8 
+            w-1/2
+            rounded-md
+            `}
+        >
             {children}
         </button>
     );
 }
 
-Button.displayName = 'Button';
-
 Button.propTypes = {
     /** Button content */
-    children: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    /** Button type */
+    type: PropTypes.oneOf(['primary', 'secondary'])
+}
+
+Button.defaultProps = {
+    type: 'primary'
 }
--- src/stories/atomic/atoms/Button.stories.js
+++ src/stories/atomic/atoms/Button.stories.js
@@ -1,15 +1,19 @@
 import React from 'react';
-
 import Button from '../../../components/atomic/atoms/Button/Button';
 
 export default {
     component: Button,
-    title: "Atoms/Button",
+    title: 'Atoms/Button',
 }
 
-const Template = args => <Button {...args} />;
-
+const Template = args => <Button {...args} type='primary' />;
 export const Default = Template.bind({});
 Default.args = {
     children: 'Primary',
+};
+
+const SecondTemplate = args => <Button {...args} type='secondary' />;
+export const Second = SecondTemplate.bind({});
+Second.args = {
+    children: 'Second',
 };
